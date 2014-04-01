2bd326dd9621fb29032bebccb52fded37e66c7fd
fabio <fabiocardoso.92@hotmail.com>
2014-03-31 22:13:13 -0300

    enviando arquivos novos

--- projetoFP/urls.py
+++ projetoFP/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'projetoFP.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'pessoas.views.index'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^pessoas/', include('pessoas.urlsPessoas')),
 )
